7e87535df6564c691b3ce635be380ae9b27d768d
wohaoe <453508901@qq.com>
2017-01-31 09:50:21 +0800

    单页面和所有页面数据

---
+++ app/index/components/collect.vue
@@ -0,0 +1,92 @@
+<template>
+    <!--收藏表单部分-->
+    <div class="alllesson">
+        <div class="lessonInfo">
+            <table>
+                <thead>
+                    <td>选课序号</td>
+                    <td>课程名称</td>
+                    <td>教师</td>
+                    <td>时间</td>
+                    <td>教室</td>
+                    <td>操作</td>
+                </thead>
+                <tr>
+                    <td>MATH120001.01</td>
+                    <td>高等数学A(上)</td>
+                    <td>徐惠平</td>
+                    <td>一 3-4 ，三 1-2 ，五 3-4</td>
+                    <td>H3308</td>
+                    <td>
+                        <button class="update add">已加入课表</button>
+                        <button class="update remove">
+                            <i class="fa fa-trash-o"></i>
+                            移除
+                        </button>
+                        <button class="update copy">复制序号</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>MATH120001.01</td>
+                    <td>高等数学A(上)</td>
+                    <td>徐惠平</td>
+                    <td>一 3-4 ，三 1-2 ，五 3-4</td>
+                    <td>H3308</td>
+                    <td>
+                        <button class="update add">已加入课表</button>
+                        <button class="update remove">
+                            <i class="fa fa-trash-o"></i>
+                            移除
+                        </button>
+                        <button class="update copy">复制序号</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>MATH120001.01</td>
+                    <td>高等数学A(上)</td>
+                    <td>徐惠平</td>
+                    <td>一 3-4 ，三 1-2 ，五 3-4</td>
+                    <td>H3308</td>
+                    <td>
+                        <button class="update add">已加入课表</button>
+                        <button class="update remove">
+                            <i class="fa fa-trash-o"></i>
+                            移除
+                        </button>
+                        <button class="update copy">复制序号</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>MATH120001.01</td>
+                    <td>高等数学A(上)</td>
+                    <td>徐惠平</td>
+                    <td>一 3-4 ，三 1-2 ，五 3-4</td>
+                    <td>H3308</td>
+                    <td>
+                        <button class="update add">已加入课表</button>
+                        <button class="update remove">
+                            <i class="fa fa-trash-o"></i>
+                            移除
+                        </button>
+                        <button class="update copy">复制序号</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>MATH120001.01</td>
+                    <td>高等数学A(上)</td>
+                    <td>徐惠平</td>
+                    <td>一 3-4 ，三 1-2 ，五 3-4</td>
+                    <td>H3308</td>
+                    <td>
+                        <button class="update add">已加入课表</button>
+                        <button class="update remove">
+                            <i class="fa fa-trash-o"></i>
+                            移除
+                        </button>
+                        <button class="update copy">复制序号</button>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
